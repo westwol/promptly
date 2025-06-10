b2af40bb855a7d107b5b2139eb35865a0a9de950
Marcos Lopez <burnarg_24@hotmail.com>
2025-06-10 13:26:17 -0600

    basic chatgpt model response (not streamed)

--- src/components/ChatConversation/ChatConversation.tsx
+++ src/components/ChatConversation/ChatConversation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Preloaded } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 
@@ -9,6 +9,25 @@ interface ChatConversationProps {
 }
 
 export const ChatConversation = ({ conversation }: ChatConversationProps) => {
+  const [body, setBody] = useState<string>("");
+  const [response, setResponse] = useState<string>("");
+
+  const onSendRequest = async () => {
+    const req = await fetch("http://localhost:4000/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        body,
+        model: "gpt-3.5-turbo",
+      }),
+    });
+    const data = await req.json();
+    console.log({ data });
+    setResponse(data?.reply || "");
+  };
+
   useEffect(() => {
     const websocket = new WebSocket("http://localhost:4000/conversation");
 
@@ -31,13 +50,19 @@ export const ChatConversation = ({ conversation }: ChatConversationProps) => {
     <div className="grid grid-rows-[1fr_100px] h-screen overflow-auto">
       <div className="text-white whitespace-pre">
         {JSON.stringify(conversation, null, 2)}
+        {response && <p>{response}</p>}
       </div>
       <div className="relative">
         <textarea
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
           className="bg-primary w-full h-full text-white outline-0"
           placeholder="type something here"
         />
-        <button className="absolute bottom-5 right-5 z-10 bg-blue-500 p-2">
+        <button
+          className="absolute bottom-5 right-5 z-10 bg-blue-500 p-2"
+          onClick={onSendRequest}
+        >
           Send
         </button>
       </div>
