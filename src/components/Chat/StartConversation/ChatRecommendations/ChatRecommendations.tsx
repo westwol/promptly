3ba1f1f0ec45d5d3cd99f7b8fdb35b9bf128d1e5
Marcos Lopez <marcos.lopez@shiphero.com>
2025-06-15 19:09:42 -0600

    remove unused code

--- src/components/Chat/StartConversation/ChatRecommendations/ChatRecommendations.tsx
+++ src/components/Chat/StartConversation/ChatRecommendations/ChatRecommendations.tsx
@@ -38,7 +38,7 @@ export const ChatRecommendations = () => {
               {currentCategory?.recommendations.map((recommendation) => (
                 <li
                   key={`conversation-starter-${recommendation}`}
-                  className="border-primary hover:bg-primary rounded-md border p-4 text-sm"
+                  className="hover:bg-primary bg-tertiary rounded-lg p-4 text-sm font-normal shadow-sm"
                   onClick={() => setContent(recommendation)}
                 >
                   {recommendation}
@@ -46,39 +46,6 @@ export const ChatRecommendations = () => {
               ))}
             </ul>
           </div>
-          {/*<div className="animate-in fade-in-50 zoom-in-95 w-full space-y-6 px-2 pt-[calc(max(15vh,2.5rem))] duration-300 sm:px-8">
-            <h2 className="text-3xl font-semibold">How can I help you?</h2>
-            <div className="flex flex-row flex-wrap gap-2.5 text-sm max-sm:justify-evenly">
-              {CHAT_STARTER_RECOMMENDATIONS.map((category) => {
-                const Icon = category.icon;
-                return (
-                  <button
-                    key={`conversation-starters-${category.label}`}
-                    className="focus-visible:ring-ring [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 border-reflect button-reflect text-primary-foreground dark:bg-primary/20 disabled:dark:hover:bg-primary/20 disabled:dark:active:bg-primary/20 outline-secondary/70 data-[selected=false]:bg-secondary/30 data-[selected=false]:text-secondary-foreground/90 data-[selected=false]:hover:bg-secondary flex h-9 items-center justify-center gap-1 rounded-xl p-2 px-5 py-2 text-sm font-semibold whitespace-nowrap shadow outline-1 backdrop-blur-xl transition-colors focus-visible:ring-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 data-[selected=false]:outline max-sm:size-16 max-sm:flex-col sm:gap-2 sm:rounded-full"
-                    onClick={() => setCurrentCategory(category)}
-                  >
-                    <Icon className="max-sm:block" />
-                    <span>{category.label}</span>
-                  </button>
-                );
-              })}
-            </div>
-            <div className="text-foreground flex flex-col">
-              {currentCategory?.recommendations.map((recommendation) => (
-                <div
-                  key={`conversation-starter-${recommendation}`}
-                  className="border-secondary/20 flex items-start gap-2 border-t py-1 first:border-none"
-                >
-                  <button
-                    className="text-secondary-foreground hover:bg-secondary/50 w-full rounded-md py-2 text-left sm:px-3"
-                    onClick={() => setContent(recommendation)}
-                  >
-                    <span>{recommendation}</span>
-                  </button>
-                </div>
-              ))}
-            </div>
-          </div>*/}
         </div>
       </div>
     </div>
